a68da12e73def3ab4547fd57550313a9fb4087c3
azn-abel <66392457+azn-abel@users.noreply.github.com>
2021-04-12 19:21:30 -0400

    fixed prefix, and launcher

--- bot.py
+++ bot.py
@@ -3,4 +3,4 @@ from discord.ext import commands
 
 TOKEN = "ODMxMzAwNjc2NTk5ODA4MDEx.YHTPFg.o8tEpy_iB6xZ8Q382h0DN9RXQmk"
 intents = discord.Intents(messages=True, guilds=True, reactions=True, members=True, presences=True)
-client = commands.Bot(command_prefix=['p!', 'P!'], intents=intents)
+client = commands.Bot(command_prefix=['z.', 'Z.'], intents=intents)
