186721a35234774e49beca7a2274efa89ccb1fec
azn-abel <66392457+azn-abel@users.noreply.github.com>
2021-04-13 13:18:06 -0400

    new slicing alg

--- database.py
+++ database.py
@@ -3,10 +3,10 @@ import os
 if os.getenv("HOME") != '/app':
     from environment import *
 
-DB_HOST = os.getenv('DATABASE_URL')[91:131]
-DB_NAME = os.getenv('DATABASE_URL')[137:]
-DB_USER = os.getenv('DATABASE_URL')[11:25]
-DB_PASS = os.getenv('DATABASE_URL')[26:90]
+DB_HOST = os.getenv('DATABASE_URL').split("/")[2].split(":")[1].split("@")[1]
+DB_NAME = os.getenv('DATABASE_URL').split("/")[-1]
+DB_USER = os.getenv('DATABASE_URL').split("/")[2].split(":")[0]
+DB_PASS = os.getenv('DATABASE_URL').split("/")[2].split(":")[1].split("@")[0]
 
 import psycopg2
 import psycopg2.extras
@@ -14,4 +14,4 @@ from psycopg2.extras import Json, DictCursor
 
 conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
 cur = conn.cursor()
-dictCur = conn.cursor(cursor_factory = DictCursor)
+dictCur = conn.cursor(cursor_factory=DictCursor)
