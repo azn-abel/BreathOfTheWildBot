fc41bd41617cb7312a4cc91d1233c386f3b877d5
azn-abel <66392457+azn-abel@users.noreply.github.com>
2021-04-13 08:32:40 -0400

    added two_col_out, images

--- data.py
+++ data.py
@@ -11,3 +11,16 @@ def get_region(region_id):
 def get_shrine(region_id, shrine_id):
     shrine = map_data['regions'][region_id]['shrines'][shrine_id]
     return shrine
+
+
+def two_col_output(lst, key):
+    output_string_1 = ""
+    output_string_2 = ""
+    counter = 0
+    for item in lst:
+        if counter < len(lst) / 2:
+            output_string_1 += f"{item[key]} - {counter}\n"
+        else:
+            output_string_2 += f"{item[key]} - {counter}\n"
+        counter += 1
+    return output_string_1, output_string_2
