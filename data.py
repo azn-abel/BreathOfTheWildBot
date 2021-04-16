638e197d090fca5bcd7ca465702251e957d518fb
Erik Svanes <erik5stars@gmail.com>
2021-04-16 15:54:13 -0400

    data changes :0

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 import json
 
 map_data = json.load(open('map.json',))
-
+gear_data = json.load(open('gear.json',))
 
 def get_region(region_id):
     region = map_data['regions'][region_id]
@@ -24,3 +24,11 @@ def two_col_output(lst, key):
             output_string_2 += f"{item[key]} - {counter}\n"
         counter += 1
     return output_string_1, output_string_2
+
+
+def get_surface(root, item):
+    return gear_data[root][item]
+
+
+def get_deep(root, column, item):
+    return gear_data[root][column][item]
