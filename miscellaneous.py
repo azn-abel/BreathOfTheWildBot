49e777fb6e802b182f156f540714ab8521af57c0
Erik Svanes <erik5stars@gmail.com>
2021-04-20 09:14:49 -0400

    fixed z.register

--- miscellaneous.py
+++ miscellaneous.py
@@ -16,10 +16,10 @@ async def register(ctx):
         return
     equippedDict = {'head': {'name': '', 'durability': 0}, 'body': {'name': '', 'durability': 0}, 'legs': {'name': '', 'durability': 0}, 'weapon': {'name': '', 'durability': 0}, 'shield': {'name': '', 'durability': 0}, 'bow': {'name': '', 'durability': 0}, 'arrows': {'name': '', 'durability': 0}}
     weaponDict = {'handheld': [{}], 'bows': [{}], 'arrows': [{}]}
-    shieldDict = [{}]
-    armorDict = [{}]
-    consumableDict = {'food': [{}], 'elixirs': [{}]}
-    key_itemsDict = [{}] # idk what we need to save here so this is a placeholder
-    dictCur.execute("INSERT INTO inventory (user_id, equipped, weapons, shields, armor, consumables, key_items) VALUES (%s, %s, %s, %s, %s, %s, %s)", (user_id, Json(equippedDict), Json(weaponDict), Json(shieldDict), Json(armorDict), Json(consumableDict), Json(key_itemsDict)))
+    shieldDict = {}
+    armorDict = {}
+    consumableDict = {'food': {}, 'elixirs': {}}
+    key_itemsDict = {} # idk what we need to save here so this is a placeholder
+    dictCur.execute("INSERT INTO inventory (user_id, rupees, equipped, weapons, shields, armor, consumables, key_items) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (user_id, 0, Json(equippedDict), Json(weaponDict), Json(shieldDict), Json(armorDict), Json(consumableDict), Json(key_itemsDict)))
     conn.commit()
     await ctx.send("You are now registered!")
